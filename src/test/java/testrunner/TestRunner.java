47bde1a3ee56a406c5d6bd83c0dfa5aba56f5c06
RivuletDom <78263954+RivuletDom@users.noreply.github.com>
2021-03-03 20:11:33 +0530

    init cucumber project

---
+++ src/test/java/testrunner/TestRunner.java
@@ -0,0 +1,17 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/java/features"
+,glue= {"stepdefinitions"}
+)
+
+
+
+public class TestRunner {
+
+}
